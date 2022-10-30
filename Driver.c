706aee3c086629b980807caaf91bfe338d4ea1e1
Quied <82113495+Quied@users.noreply.github.com>
2022-10-30 17:08:45 +0200

    Add files via upload

--- Driver.c
+++ Driver.c
@@ -3,18 +3,24 @@
 
 #pragma warning (disable : 4100)
 
-NTSTATUS DriverEntry(PDRIVER_OBJECT pDriveObj,
-	PUNICODE_STRING pRegistKey) {
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriveObj, PUNICODE_STRING pRegistKey) {
 	pDriveObj->DriverUnload = UnloadDriver;
 	DbgPrintEx(0, 0, "message");
 
 	DebugMessage("Driver Load");
 
+
+
 	return STATUS_SUCCESS;
 }
 
+
+
 NTSTATUS UnloadDriver(PDRIVER_OBJECT pDriverOvb) {
 	DebugMessage("Driver UnLoad");
+
+
 
 	return STATUS_SUCCESS;
 }
